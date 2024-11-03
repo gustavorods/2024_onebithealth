3bd3355d51b9ae6f901a283230aaa045bb018902
gustavorods <gustavo.rlsilva07@gmail.com>
2024-11-03 04:40:09 -0300

    feat: share button working

--- src/components/Form/ResultImc/style.js
+++ src/components/Form/ResultImc/style.js
@@ -18,6 +18,22 @@ const styles = StyleSheet.create({
         fontSize:18,
         color:'#FF0046',
         fontWeight:'bold',
+    },
+    boxShareButton: {
+        width:"100%",
+        alignItems:"center",
+        marginBottom:10,
+    },
+    shared: {
+        backgroundColor:"#1877f2",
+        borderRadius:50,
+        paddingBottom:5,
+        paddingTop:5,
+    },
+    sharedText: {
+        color:"#FFFFFF",
+        fontWeight:"bold",
+        paddingHorizontal:30,
     }
  });
 
